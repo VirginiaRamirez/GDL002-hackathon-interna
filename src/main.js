512130a4ce0df698278fdab3fbf194ba2332ff5c
Monse0051 <rouge0051@gmail.com>
2019-03-14 15:31:03 -0600

    Agregamosn funciones para visualizar la data en los campos

--- src/main.js
+++ src/main.js
@@ -9,10 +9,9 @@ function fillElements (movieList, divElement){
     for(let i=0; i<movieList.length; i++) {
         let divMovie = document.createElement("div");
         divMovie.className = "movie";
-        divMovie.innerHTML = "<button><img src='" + movieList[i].Poster + "'></button>" + "<p> </p>"+ 
-                            movieList[i].Title;
+        divMovie.innerHTML = "<button><img src='" + movieList[i].Poster + "'></button>";
                       
-        // callback function that prints/shows pokemon information
+        
         divMovie.addEventListener("click", ()=>showMovieInfo(movieList[i]));
         divElement.insertAdjacentElement("beforeend", divMovie);
     }
@@ -22,6 +21,8 @@ function fillElements (movieList, divElement){
 function showMovieInfo(movie){
     document.getElementById("welcomeCarrousel").style.display = "none";
     document.getElementById("welcomeScreen").style.display = "none";
+    document.getElementById("topClassicsScreen").style.display= "none";
+    document.getElementById("topModernScreen").style.display= "none"
     document.getElementById("movieDetailScreen").style.display = "block";
   
     let divMovieInfo = document.getElementById("movieDetailScreen");
@@ -29,13 +30,14 @@ function showMovieInfo(movie){
   
      movieScreen[0].innerHTML = "Título: " + movie.Title;
      movieScreen[1].innerHTML = "<img src='" +  movie.Poster + "'>";
-     movieScreen[2].innerHTML = "Año: " + movie.Year;
-     movieScreen[3].innerHTML = "Genero: " + movie.Genre;
+     movieScreen[2].innerHTML = movie.Runtime;
+     movieScreen[3].innerHTML = "Director: " + movie.Director;
+     movieScreen[4].innerHTML = "Rating: " + movie.imdbRating;
+     movieScreen[5].innerHTML = "Plot: " + movie.Plot;
    
+      
 }
   
-  
-
 let topHorrorMoviesIds =  window.moviesHandler.topHorrorMovieIds;
 
 for (let index = 0; index <  topHorrorMoviesIds.length; index++) {
@@ -49,10 +51,71 @@ for (let index = 0; index <  topHorrorMoviesIds.length; index++) {
             // convertimos nuestra data a JSON
             return data.json();
         }).then((dataAsJSON) => {
-            // cualquier operación con la data ya procesada
-            console.log(dataAsJSON);
-
+           
             fillElements([dataAsJSON], document.getElementById("welcomeCarrousel"));
         }
     );
 }
+
+function topClasicHorrorMovies() {
+    document.getElementById("welcomeCarrousel").style.display= "none";
+    document.getElementById("movieDetailScreen").style.display = "none";
+    document.getElementById("topClassicsScreen").style.display= "block";
+let topClasicHorrorMoviesIds =  window.moviesHandler.topClasicHorrorMoviesIds;
+
+for (let index = 0; index <  topClasicHorrorMoviesIds.length; index++) {
+    const movieId = topClasicHorrorMoviesIds[index];
+    let url = new URL(MOVIE_API_URL);
+
+    let urlDataRequest = new URL(`?i=${movieId}&apikey=${KEY}`, url);
+    
+    fetch(urlDataRequest)
+        .then((data) => {
+            // convertimos nuestra data a JSON
+            return data.json();
+        }).then((dataAsJSON) => {
+            
+            fillElements([dataAsJSON], document.getElementById("topClassicsScreen"));
+        }
+    );
+}
+                                }
+document.getElementById("topClassicsBtn").addEventListener("click", topClasicHorrorMovies);
+
+function topModernHorrorMovies() {
+    document.getElementById("welcomeCarrousel").style.display= "none";
+    document.getElementById("movieDetailScreen").style.display = "none";
+    document.getElementById("topClassicsScreen").style.display= "none";
+    document.getElementById("topModernScreen").style.display= "block";
+let topModernHorrorMoviesIds =  window.moviesHandler.topModernHorrorMoviesIds;
+
+for (let index = 0; index <  topModernHorrorMoviesIds.length; index++) {
+    const movieId = topModernHorrorMoviesIds[index];
+    let url = new URL(MOVIE_API_URL);
+
+    let urlDataRequest = new URL(`?i=${movieId}&apikey=${KEY}`, url);
+    
+    fetch(urlDataRequest)
+        .then((data) => {
+            // convertimos nuestra data a JSON
+            return data.json();
+        }).then((dataAsJSON) => {
+            
+            fillElements([dataAsJSON], document.getElementById("topModernScreen"));
+        }
+    );
+}
+                                }
+document.getElementById("topModernBtn").addEventListener("click", topModernHorrorMovies);
+
+const startingScreen =()=> {
+
+    document.getElementById ("welcomeScreen").style.display="flex";
+    document.getElementById ("welcomeCarrousel").style.display="flex";
+    document.getElementById ("movieDetailScreen").style.display="none";
+    document.getElementById ("notFoundScreen").style.display="none";
+    document.getElementById ("topClassicsScreen").style.display="none";
+    document.getElementById ("topModernScreen").style.display="none";
+    };
+ 
+    document.getElementById ("homeBtn").addEventListener("click",startingScreen);
